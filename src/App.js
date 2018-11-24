15e5afb6897718cdbfe175422ab034d66f16daa0
jennifer <teriyakibuns@yahoo.com>
2018-11-23 16:47:39 -0800

    added favorites and logic for filtering favorites

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import Listing from "./components/Listing";
+
 import "./App.css";
+
+library.add(faStar, faQuestionCircle)
+
 
 class App extends Component {
   render() {
--- src/components/ModalDetail.js
+++ src/components/ModalDetail.js
@@ -1,10 +1,25 @@
 import React from "react";
 import axios from "axios";
 import { Modal, Button, Carousel } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../images/fair_alt.jpg";
 import imageAlt from "../images/nopreview.jpg";
 import "./ModalDetail.css";
+
+let notFavorite = {
+  marginLeft: "85%",
+  fontSize: "20px",
+  color: "#ff7843",
+  opacity: ".2"
+};
+
+let favorite = {
+  marginLeft: "85%",
+  fontSize: "20px",
+  color: "#ff7843",
+  opacity: "100"
+};
 
 class ModalDetail extends React.Component {
   constructor(props) {
@@ -47,11 +62,18 @@ class ModalDetail extends React.Component {
   };
 
   render() {
+    let mileString =
+      this.state.car &&
+      this.state.car.mileage
+        .toString()
+        .split(/(?=(?:\d{3})+(?:\.|$))/g)
+        .join(",");
+
     let carouselImages =
       this.state.car &&
       this.state.car.image_location_list.map((image, i) => {
         return (
-          <Carousel.Item>
+          <Carousel.Item key={i}>
             <img
               width={550}
               alt="carousel"
@@ -70,25 +92,42 @@ class ModalDetail extends React.Component {
         <Modal.Body>
           {this.state.requestDidError ? (
             <div>
-              <h3>Sorry, there was an error when we attempted to get this vehicle's data. :(</h3>
+              <h3>
+                Sorry, there was an error when we attempted to get additional
+                data for this vehicle. :(
+              </h3>
             </div>
           ) : (
             <div>
               <Carousel interval={null}>{carouselImages}</Carousel>
               <div className="modal-top-details">
-                <span className="modal-year-make">
-                  {this.state.car && this.state.car.model_year}{" "}
-                  {this.state.car && this.state.car.make}
-                </span>
-                <br />
-                <span className="modal-model-trim">
-                  {this.state.car && this.state.car.model}{" "}
-                  {this.state.car && this.state.car.trim}
-                </span>
-                <br />
-                <span className="modal-miles">
-                  {this.state.car && this.state.car.mileage} miles
-                </span>
+                <div className="modal-icon-div">
+                  <FontAwesomeIcon
+                    icon="star"
+                    className="modal-star-icon"
+                    style={
+                      this.props.favCars.includes(this.state.car.id)
+                        ? favorite
+                        : notFavorite
+                    }
+                    onClick={this.props.toggleFav}
+                  />
+                </div>
+                <div className="modal-detail-text">
+                  <span className="modal-year-make">
+                    {this.state.car && this.state.car.model_year}{" "}
+                    {this.state.car && this.state.car.make}
+                  </span>
+                  <br />
+                  <span className="modal-model-trim">
+                    {this.state.car && this.state.car.model}{" "}
+                    {this.state.car && this.state.car.trim}
+                  </span>
+                  <br />
+                  <span className="modal-miles">
+                    {mileString && mileString} miles
+                  </span>
+                </div>
               </div>
               <div className="modal-center-div" />
               <div className="modal-payment-details">
