3f7c576264fa06ef7f3272a6fb654c587f76d35a
jennifer <teriyakibuns@yahoo.com>
2018-11-25 10:22:28 -0800

    Updates

---
+++ precache-manifest.0e00063c43219d391e9c449d5de6fc6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b79bfc3c3b997b915640f0ec43d0e96",
+    "url": "/fair/static/media/fair_alt.6b79bfc3.jpg"
+  },
+  {
+    "revision": "9185512be263fe3b4b3e10379bd3cc59",
+    "url": "/fair/static/media/car_lineup.9185512b.png"
+  },
+  {
+    "revision": "475defc1b5ef8a58ab9d",
+    "url": "/fair/static/js/runtime~main.475defc1.js"
+  },
+  {
+    "revision": "566c5c1f9db16f56f920",
+    "url": "/fair/static/js/main.566c5c1f.chunk.js"
+  },
+  {
+    "revision": "3885e761cdd9341d11ee",
+    "url": "/fair/static/js/1.3885e761.chunk.js"
+  },
+  {
+    "revision": "566c5c1f9db16f56f920",
+    "url": "/fair/static/css/main.4913d18e.chunk.css"
+  },
+  {
+    "revision": "892972300f63ef1ae8cba9f27ffe4e84",
+    "url": "/fair/index.html"
+  }
+];
